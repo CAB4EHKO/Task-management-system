18ca7e9f9c6b887cff81db62f5aa9a1983efb628
Savchenko <savchenko.ra@yandex.ru>
2024-12-03 22:45:12 +0300

    Написал модели данных.

---
+++ src/main/java/ru/tmsystem/tms/enums/Priority.java
@@ -0,0 +1,5 @@
+package ru.tmsystem.tms.enums;
+
+public enum Priority {
+    PRIORITY_LOW, PRIORITY_MEDIUM, PRIORITY_HIGH
+}
---
+++ src/main/java/ru/tmsystem/tms/enums/Status.java
@@ -0,0 +1,5 @@
+package ru.tmsystem.tms.enums;
+
+public enum Status {
+    STATUS_PENDING, STATUS_RUNNING, STATUS_FINISHED
+}
